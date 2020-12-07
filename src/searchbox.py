9a02c973a14b7ec011c819d648ab01a41b5c66ec
Jerry Wei <junyu.wei@student.unsw.edu.au>
2020-12-07 21:37:32 +1100

    add api for sort based on perc or num

--- src/searchbox.py
+++ src/searchbox.py
@@ -5,25 +5,33 @@ https://stackoverflow.com/questions/50606250/how-do-i-return-a-word-using-the-tk
 from tkinter import*
 from gui_table import Table
 from class_scraper import Class_scrapter
+from enum import Enum
+
+class Opt(int, Enum):
+    UGRD_or_PGRD = 0
+    PERC_or_NUM = 1
+
 
 class Searchbox:
     def __init__(self):
         self.entry = None
         self.resultox = None
         self.acronym_dictionary = {}
-        self.var = None
+        self.vars = {Opt.UGRD_or_PGRD:None, Opt.PERC_or_NUM:None}
         self.master=Tk()
         self.master.title("The Acronym Search Engine")
         self.master.geometry('300x100')
 
-    def return_entry(self, en):
+    def return_entry(self, tmp):
         
         content = self.entry.get()
-        is_undergrad = bool("under" in self.var.get())
+        under_or_post = bool("under" in self.vars[Opt.UGRD_or_PGRD].get())
+
         try:
             self.resultox.delete(0,END)
             self.resultox.insert(0,"FOUND")
-            c = Class_scrapter(content, is_undergrad)
+            c = Class_scrapter(content, under_or_post)
+
         except:
             self.resultox.delete(0,END)
             self.resultox.insert(0,"404 NOT FOUND")
@@ -34,26 +42,35 @@ class Searchbox:
         # print(res)
         #print(bool("under" in self.var.get()))
         pass
-    def create_dropdown(self):
-        OPTIONS = [
+    def create_dropdown(self, which=0):
+        grad_options = [
             "undergrad",
             "postgard"
         ]
+        perc_options = [
+            "percentage",
+            "total_number"
+        ]
+        options_dict = {
+            Opt.UGRD_or_PGRD:grad_options,
+            Opt.PERC_or_NUM:perc_options
+        }
+        OPTIONS = options_dict[which]
         #
-        self.var = StringVar(self.master)
-        self.var.set(OPTIONS[0]) # default value
+        var = StringVar(self.master)
+        var.set(OPTIONS[0]) # default value
 
-        OptionMenu(self.master, self.var, *OPTIONS).grid(row=1, sticky=W)
+        OptionMenu(self.master, var, *OPTIONS).grid(row=1, sticky=W)
 
         self.entry=Entry(self.master)
         self.entry.grid(row=1, column=1)
-        self.var.trace('w', self.cb_drop_down_menu)
-
+        var.trace('w', self.cb_drop_down_menu)
+        self.vars[which] = var
         #
     def create_search_box(self):
 
         self.create_dropdown()
-        
+
         Label(self.master, text="Search box:").grid(row=0, sticky=W)
         self.entry=Entry(self.master)
         self.entry.grid(row=0, column=1)
